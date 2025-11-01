0e27f579f3bc43d5050c1a0c305cfb4cba3bba9f
Aidar Khabirov <khabirov.ar1990@gmail.com>
2025-11-02 03:23:04 +0500

    Add prefix and tag for routers

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,16 +4,19 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas
 from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix = "/posts",
+    tags   = ["Posts"]
+)
 
-@router.get('/posts', response_model=List[schemas.Post])
+@router.get('/', response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
     return posts
 
-@router.post('/posts', status_code=status.HTTP_201_CREATED, response_model=schemas.Post) # change default status in decorator
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post) # change default status in decorator
 def createposts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
     #                (post.title, post.content, post.published))
@@ -26,7 +29,7 @@ def createposts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     db.refresh(new_post) # same as RETURNING
     return new_post
 
-@router.get('/posts/{id}', response_model=schemas.Post)
+@router.get('/{id}', response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)): # add ": int" for validation, id must be integer and will be converted in integer
     # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id),))
     # post = cursor.fetchone()
@@ -36,7 +39,7 @@ def get_post(id: int, db: Session = Depends(get_db)): # add ": int" for validati
     print(post)
     return post
 
-@router.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
     # post = cursor.fetchone()
@@ -51,7 +54,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put('/posts/{id}', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Post)
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Post)
 def update_post(id: int, new_post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute(""" UPDATE posts SET title=%s, content=%s, published=%s WHERE id = %s RETURNING * """,
     #                (new_post.title, new_post.content, new_post.published, str(id)))
