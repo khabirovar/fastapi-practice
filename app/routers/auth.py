5164cd2e27edf89b475996b113a1fd4cd7951df3
Aidar Khabirov <khabirov.ar1990@gmail.com>
2025-11-02 21:46:50 +0500

    Switch login creds from body to form data

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from .. import models, schemas, utils, oauth2
 from ..database import get_db
 
@@ -8,8 +9,8 @@ router = APIRouter(
 )
 
 @router.post("/login")
-def login(creds: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == creds.email).first()
+def login(creds: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == creds.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
     
